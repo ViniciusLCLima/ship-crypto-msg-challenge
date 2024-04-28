c9f3a09b582d697f5de3c203a13e23275b4493f6
Vinicius Lima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-04-28 18:40:27 +0000

    Initializing

--- main.cs
+++ main.cs
@@ -2,6 +2,12 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.ReadLine ("Enter cryptographed message:");
+    Console.Write("Enter cryptographed message:\n");
+    string msg = Console.ReadLine ();
+    string[] eightParts = msg.split(" ")
+    foreach (string eightPart in eightParts){
+      string first4of8 = eight.Substring(i, 4)
+      
+    }
   }
 }
